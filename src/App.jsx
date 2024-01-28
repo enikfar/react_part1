6cc7df9f9d94ad1f65987187c6ba0434f2ac7366
Ehsan Nikfar <100062123+enikfar@users.noreply.github.com>
2024-01-28 17:53:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import Header from './header'
 import Content from './content'
 import Total from './total'
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1><Header course = {course} /> </h1>
-      <p>
-        <Content part = {part1} exercises = {exercises1} />
-      </p>
-      <p>
-      <Content part = {part2} exercises = {exercises2} />
-      </p>
-      <p>
-      <Content part = {part3} exercises = {exercises3} />
-      </p>
-      <p>Number of exercises <Total items={[exercises1, exercises2, exercises3]} /></p>
-
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <p> the sum of the exercises is <Total parts={course.parts} /></p>
     </div>
   )
 }
-
 export default App
